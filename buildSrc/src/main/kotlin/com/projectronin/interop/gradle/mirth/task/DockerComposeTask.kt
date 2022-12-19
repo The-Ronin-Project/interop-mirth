70fdddbeae7767674ebaf54d0aa86fd4c9de5d8f
Sam Felt <sfelt@projectronin.com>
2022-12-19 12:41:41 -0800

    [INT-1319] Add conditional checks to dockerCompose (#105)
    * [INT-1319] Add conditional checks to dockerCompose
    
    * remove non-used check

--- buildSrc/src/main/kotlin/com/projectronin/interop/gradle/mirth/task/DockerComposeTask.kt
+++ buildSrc/src/main/kotlin/com/projectronin/interop/gradle/mirth/task/DockerComposeTask.kt
@@ -3,6 +3,7 @@ package com.projectronin.interop.gradle.mirth.task
 import com.projectronin.interop.gradle.mirth.mirth
 import org.gradle.api.tasks.TaskAction
 import org.gradle.process.ExecOperations
+import java.io.ByteArrayOutputStream
 import javax.inject.Inject
 
 /**
@@ -11,16 +12,25 @@ import javax.inject.Inject
 open class DockerComposeTask @Inject constructor(private val execOperations: ExecOperations) : BaseMirthTask() {
     @TaskAction
     fun up() {
-        logger.lifecycle("Removing stopped containers")
+        val os = ByteArrayOutputStream()
+        val alreadyRunningCommand = "docker compose ps -q mc --status running"
         execOperations.exec {
             workingDir(project.mirth().dockerDirectory.get())
+            standardOutput = os
             isIgnoreExitValue = true
-            commandLine("docker compose rm --force --volumes mockehrinit".split(" "))
+            commandLine(alreadyRunningCommand.split(" "))
         }
-        logger.lifecycle("Running Docker compose")
-        execOperations.exec {
-            workingDir(project.mirth().dockerDirectory.get())
-            commandLine("docker compose up -d --quiet-pull".split(" "))
+        val result = os.toString()
+        val notRunning = (result.isNullOrBlank() == true)
+        logger.lifecycle("$alreadyRunningCommand == $result")
+        logger.lifecycle("notRunning == $notRunning")
+
+        if (notRunning) {
+            logger.lifecycle("Running Docker compose")
+            execOperations.exec {
+                workingDir(project.mirth().dockerDirectory.get())
+                commandLine("docker compose up -d --quiet-pull".split(" "))
+            }
         }
         logger.lifecycle("Docker compose completed")
     }
