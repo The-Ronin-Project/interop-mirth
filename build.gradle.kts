46b3c71d2e697752723cd86180ea52eab8bebb96
Josh Smith <43484632+arssycro@users.noreply.github.com>
2023-07-13 17:06:34 -0500

    Add missing Version Catalog Update plugin (#335)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.interop.version)
+    alias(libs.plugins.interop.version.catalog)
     alias(libs.plugins.interop.integration) apply false
     alias(libs.plugins.interop.junit) apply false
     alias(libs.plugins.interop.publish) apply false
